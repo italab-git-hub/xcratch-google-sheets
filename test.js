32dd3061089e271916a9da5c7068ddda959d269f
italab-git-hub <itagaki@italab.info>
2025-03-21 15:01:06 +0900

    test.js

--- test.js
+++ test.js
@@ -1,1 +1,57 @@
+export default function(Scratch) {
+  class SheetGAS {
+    getInfo() {
+      return {
+        id: 'sheetgas',
+        name: 'Google Sheets (GAS)',
+        blocks: [
+          {
+            opcode: 'readCell',
+            blockType: Scratch.BlockType.REPORTER,
+            text: 'GAS [url] の [シートID] / [シート名] / [セル] を読む',
+            arguments: {
+              url: { type: Scratch.ArgumentType.STRING, defaultValue: 'https://script.google.com/macros/s/AKfycbwI27iHVqmprJ-oEvLa2pLJZo7Hj5FJVh4jA2-VWu0LLQyzO4yRfFHJNS40JqY4Y1bc/exec' },
+              sheetId: { type: Scratch.ArgumentType.STRING, defaultValue: '1BdTPdUJGUs4NzyQLVhi_FAPnpOC7oGXD7jVk_Trk5vY' },
+              sheetName: { type: Scratch.ArgumentType.STRING, defaultValue: 'Sheet1' },
+              cell: { type: Scratch.ArgumentType.STRING, defaultValue: 'A1' }
+            }
+          },
+          {
+            opcode: 'writeCell',
+            blockType: Scratch.BlockType.COMMAND,
+            text: 'GAS [url] に [値] を [シートID] / [シート名] / [セル] に書く',
+            arguments: {
+              url: { type: Scratch.ArgumentType.STRING, defaultValue: 'https://script.google.com/macros/s/AKfycbwI27iHVqmprJ-oEvLa2pLJZo7Hj5FJVh4jA2-VWu0LLQyzO4yRfFHJNS40JqY4Y1bc/exec' },
+              sheetId: { type: Scratch.ArgumentType.STRING, defaultValue: 'スプレッドシートID' },
+              sheetName: { type: Scratch.ArgumentType.STRING, defaultValue: 'Sheet1' },
+              cell: { type: Scratch.ArgumentType.STRING, defaultValue: 'A1' },
+              value: { type: Scratch.ArgumentType.STRING, defaultValue: 'Hello' }
+            }
+          }
+        ]
+      };
+    }
+
+    async readCell(args) {
+      const url = `${args.url}?sheetId=${args.sheetId}&sheetName=${args.sheetName}&cell=${args.cell}`;
+      const res = await fetch(url);
+      return await res.text();
+    }
+
+    async writeCell(args) {
+      const form = new FormData();
+      form.append("sheetId", args.sheetId);
+      form.append("sheetName", args.sheetName);
+      form.append("cell", args.cell);
+      form.append("value", args.value);
+
+      await fetch(args.url, {
+        method: "POST",
+        body: form
+      });
+    }
+  }
+
+  return SheetGAS;
+}
 
